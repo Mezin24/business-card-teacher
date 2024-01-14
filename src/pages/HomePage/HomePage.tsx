8c1c42ed95a1b38ca9df40087ef80a97369e8473
Pavel Mezencev <mezencev24@mail.ru>
2024-01-14 13:42:29 +0300

    add header

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,10 @@
+import Header from '@/components/Header';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 export default HomePage;
